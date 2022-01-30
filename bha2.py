fc7efa544ce6a43d60fffd2b8a3ad2c402b36701
MarcelinkaD <97838551+MarcelinkaD@users.noreply.github.com>
2022-01-30 10:46:02 +0100

    bambus 2.0

--- bha2.py
+++ bha2.py
@@ -1,14 +1,33 @@
+import math 
+
 def main():
-    n = int(input())
-    rozklad = []
+    do_wyprintowania = int(input())
+    licznik = 1
+    taka_pomocna_zmienna = do_wyprintowania
     dzielnik = 2
-    while(n != 1):
-        if(n % dzielnik == 0):
-            rozklad.append(dzielnik)
-            n //= dzielnik
-        else:
-            dzielnik += 1
-            
-    print(rozklad)
+    poprzednia = 1
     
+    while taka_pomocna_zmienna != 0:
+        n = licznik
+        sqrt = math.sqrt(licznik)
+        
+        while(dzielnik <= 5): 
+            if(n % dzielnik == 0):
+                n //= dzielnik
+
+            else:
+                dzielnik += 1
+
+                
+                
+                
+        if(n == 1):
+            print(str(licznik) + " z="+ str(taka_pomocna_zmienna) + " roznica=" + str(licznik - poprzednia))
+            poprzednia = licznik
+            taka_pomocna_zmienna -= 1
+            
+        licznik += 1
+        dzielnik = 2
+   
+
 main()
