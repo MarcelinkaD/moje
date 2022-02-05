2697c4e747d1a23e4276209b0126d6cfcd5a8c45
MarcelinkaD <97838551+MarcelinkaD@users.noreply.github.com>
2022-02-05 13:56:32 +0100

    bitwa pod wiedniem

---
+++ bpw.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+@dataclass
+class Choragiew:    
+    liczba: int
+    suma: int
+    liczba_napis : str
+    
+
+def main():
+    #ch = Choragiew(1000,1,"1000")
+    tab = []
+    
+    ilosc_ch = int(input())
+    choraza = list(map(str, input().split()))
+    
+    for i in choraza:
+        wynik = 0
+        for k in i:
+            wynik += int(k)
+            
+        ch = Choragiew(int(i), wynik, i)
+        tab.append(ch)
+    
+    tab.sort(key = lambda x: (x.suma, x.liczba), reverse=True)
+    
+    
+    
+    for obj in tab:
+        print(obj.liczba, end = " ")
+    
+        
+main()
