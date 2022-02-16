35ff4038cf21f7842daa18085bd9d689fc032b75
MarcelinkaD <97838551+MarcelinkaD@users.noreply.github.com>
2022-02-16 17:35:54 +0100

    gasienica w innym wydaniu - szukanie minimum - zlodzieje

---
+++ zlo.py
@@ -0,0 +1,45 @@
+from sys import stdin
+input = stdin.readline
+
+def gasienica(tab, dlugosc_tab, do_kradziezy):
+    #GLOWA i ogon musza byc ustawione przed poczatkiem tablicy
+    ogon = -1
+    glowa = -1
+    wynik_najlepszy = dlugosc_tab + 1
+    akt_wynik = 0
+
+    while (ogon < dlugosc_tab - 1):
+        while (glowa < dlugosc_tab - 1) and (akt_wynik <= do_kradziezy):
+            glowa += 1
+            akt_wynik += tab[glowa]
+            
+            if(akt_wynik == do_kradziezy):
+                wynik_najlepszy = min(glowa - ogon, wynik_najlepszy)
+        
+        ogon += 1
+        akt_wynik -= tab[ogon]
+        
+        #jezeli szukamy minialnej dlugosci to warunek powtarzamy gdy ogon "skraca" odcinek (czyli sie przesuwa do przodu)
+        #bo wtedy mozemy uzuskac krotszy odcinek
+        if(akt_wynik == do_kradziezy):
+            wynik_najlepszy = min(glowa - ogon, wynik_najlepszy)
+        
+    
+    return wynik_najlepszy
+    
+def main():
+    liczba_grzadek, liczba_do_zabrania = map(int, input().split())
+    grzadki = list(map(int, input().split()))
+    wynik = 0
+    
+    if liczba_do_zabrania in grzadki:
+        return "1"
+    else:
+        wynik = int(gasienica(grzadki, liczba_grzadek, liczba_do_zabrania))
+        
+    if wynik == liczba_grzadek + 1:
+        return "Nie"
+    else:
+        return wynik
+    
+print(main())
