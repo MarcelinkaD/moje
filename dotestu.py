d261beb0fc33d4be47009b9bef138f9cd6aec329
MarcelinkaD <97838551+MarcelinkaD@users.noreply.github.com>
2023-04-02 12:22:44 +0200

    do testowania + jakiś randomowy txt

--- dotestu.py
+++ dotestu.py
@@ -1,28 +1,51 @@
 import math
 import heapq
+from collections import deque
 from collections import Counter as C
 from itertools import accumulate
 from dataclasses import dataclass
 from sys import stdin
 input = stdin.readline
 
-def lastStoneWeight(stones):
-        kol = []
+# przykładowy graf reprezentowany przez słownik sąsiedztwa
+graph = {
+    'a': ['b', 'c'],
+    'b': ['d', 'e'],
+    'c': ['f'],
+    'd': [],
+    'e': ['f'],
+    'f': []
+}
 
-        for i in stones:
-            heapq.heappush(kol, -1 * i)
+def bfs_path(graph, start, end):
+    # kolejka przechowująca wierzchołki do odwiedzenia
+    queue = deque([start])
+    # słownik zapisujący poprzedników wierzchołków
+    predecessors = {start: None}
 
-        while len(kol) > 1:
-            k1 = -heapq.heappop(kol)
-            k2 = -heapq.heappop(kol)
-            
-            if k1 != k2:
-                heapq.heappush(kol, -(max(k1, k2) - min(k1, k2)))
-            else:
-                continue
-            
-        if len(kol) == 0:
-            return 0
-        return abs(kol[0])
+    while queue:
+        # pobierz pierwszy wierzchołek z kolejki
+        current = queue.popleft()
 
-lastStoneWeight([3,7,2])
+        # jeśli dotarliśmy do wierzchołka końcowego, zakończ BFS
+        if current == end:
+            break
+
+        # przeglądaj sąsiadów wierzchołka bieżącego
+        for neighbor in graph[current]:
+            if neighbor not in predecessors:
+                # zapisz poprzednika sąsiada i dodaj go do kolejki
+                predecessors[neighbor] = current
+                queue.append(neighbor)
+
+    # zapisz ścieżkę od wierzchołka końcowego do wierzchołka początkowego
+    path = []
+    while end:
+        path.append(end)
+        end = predecessors[end]
+
+    # odwróć kolejność wierzchołków, aby uzyskać ścieżkę od początku do końca
+    return path[::-1]
+
+# przykładowe wywołanie funkcji bfs_path
+print(bfs_path(graph, 'a', 'f'))  # wynik: ['a', 'c', 'f']
