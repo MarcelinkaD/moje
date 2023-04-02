4ec851f10ac3a3ec37ccbfb87812f38a02a45942
MarcelinkaD <97838551+MarcelinkaD@users.noreply.github.com>
2023-04-02 12:22:19 +0200

    niedokończone

---
+++ porachunki.py
@@ -0,0 +1,21 @@
+# https://szkopul.edu.pl/c/olimpiada-od-podstaw-2022-23/p/por/18448/
+
+from sys import stdin
+input = stdin.readline
+
+def main():
+    n = int(input())
+    pracownicy = []
+    
+    for _  in range(n):
+        a, b = map(int, input().split())
+        pracownicy.([a, b])
+        
+    do_rozdania = pracownicy[0][0] - pracownicy[0][1]
+    i = 0
+    w = 0
+    
+    while do_rozdania != 0:
+        if do_rozdania 
+            
+main()
